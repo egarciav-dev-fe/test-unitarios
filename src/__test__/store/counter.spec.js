70ce95e355d8921746b11e1f92488f0f25a49bb8
egarciav-dev-fe <egarciav.dev.fe@gmail.com>
2024-10-07 23:35:25 -0300

    Ejercicio 1 realizado con pruebas unitarias correspondientes

---
+++ src/__test__/store/counter.spec.js
@@ -0,0 +1,30 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+
+import { useCounterStore } from '@/stores/counter'
+
+import { mount } from '@vue/test-utils'
+
+describe('useCounter', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  it('Contador inicial cero', () => {
+    const initialCounter = useCounterStore()
+
+    expect(initialCounter.count).toBe(0)
+  })
+
+  it('Incrementar contador', () => {
+    const initialCounter = useCounterStore()
+    initialCounter.increment()
+    expect(initialCounter.count).toBe(1)
+  })
+  it('Disminuir contador', () => {
+    const initialCounter = useCounterStore()
+    initialCounter.decrement()
+    console.log(initialCounter.decrement())
+    expect(initialCounter.count).toBeLessThan(0)
+  })
+})
